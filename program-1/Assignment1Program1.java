1fe2485305702afbdc1a07ef35f2a0ce6004f179
Sleepy-Fox <81337708+Sleepy-Fox@users.noreply.github.com>
2026-02-26 09:51:24 -0500

    Print Initial
    Added initials print.

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,5 +1,16 @@
 public class Assignment1Program1 {
   public static void main(String[] args) {
 
+    System.out.println("KK        KK      WW                              WW");
+    System.out.println("KK      KK        WW              WW              WW");
+    System.out.println("KK    KK          WW            WW  WW            WW");
+    System.out.println("KK  KK            WW          WW      WW          WW");
+    System.out.println("KK    KK          WW        WW          WW        WW");
+    System.out.println("KK      KK        WW      WW              WW      WW");
+    System.out.println("KK        KK      WW    WW                  WW    WW");
+    System.out.println("KK          KK    WW  WW                      WW  WW");
+    System.out.println("KK           KK   WW                              WW");
+
+
   }
 }
