c21c511f024833dedb152840cd491a7d682f74ac
Sleepy-Fox <81337708+Sleepy-Fox@users.noreply.github.com>
2026-02-26 12:40:34 -0500

    Added Temperature and Random Number Generator.

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,5 +1,6 @@
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class Assignment1Program1 {
   public static void main(String[] args) {
@@ -7,7 +8,12 @@ public class Assignment1Program1 {
     Scanner scan = new Scanner (System.in) ;
     StringBuilder rev = new StringBuilder();
 
+    Random randomGenerator = new Random();
+
     String userInput;
+    int userFahrenheit;
+    int Celsius;
+    int randomNum;
 
 
     System.out.println("KK        KK      WW                              WW");
@@ -25,7 +31,13 @@ public class Assignment1Program1 {
     userInput = rev.append(userInput).reverse().toString();
     userInput = userInput.substring(1, 4);
 
-    System.out.println("The reverse of the string is: " + userInput);
+    System.out.println("Enter a Fahrenheit temperature:");
+    userFahrenheit = scan.nextInt();
+    Celsius = (userFahrenheit - 32) * 5 / 9;
+
+    randomNum = randomGenerator.nextInt(16384 - 32 + 1) + 32;
+
+    System.out.println("Your new string is " + Celsius + userInput + randomNum);
 
 
 
